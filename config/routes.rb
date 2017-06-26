c9ffc5a2a5e43e3edede502e1f90e0cedcbd84bb
Tobias Schmidt <t.schmidt@rubidat.de>
2017-06-26 12:00:35 +0200

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy', as: 'signout'
+  put '/set_per_page', to: 'sessions#set_per_page'
+  resources :users, path: '/admin/users'
 end
