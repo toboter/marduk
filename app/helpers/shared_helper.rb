1cbcaab42392c51e453244a0d73ee9e0ff6f27e2
Tobias Schmidt <t.schmidt@rubidat.de>
2017-06-30 13:46:26 +0200

    fixing current_user error

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -6,7 +6,7 @@ module SharedHelper
 
 
   def can_create?
-    current_user && current_user.app_creator || current_user.app_admin
+    current_user && (current_user.app_creator || current_user.app_admin)
   end
 
   def can_administrate?
@@ -15,12 +15,12 @@ module SharedHelper
 
   # extend Nabu
   def can_comment?
-    current_user && current_user.app_commentator || current_user.app_admin
+    current_user && (current_user.app_commentator || current_user.app_admin)
   end
   # end extend Nabu
 
   def can_publish?
-    current_user && current_user.app_publisher || current_user.app_admin
+    current_user && (current_user.app_publisher || current_user.app_admin)
   end
   # end extend Enki
 
--- lib/marduk/version.rb
+++ lib/marduk/version.rb
@@ -1,3 +1,3 @@
 module Marduk
-  VERSION = '0.0.5.beta'
+  VERSION = '0.0.6.beta'
 end
